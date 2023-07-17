083f99ce56f91cadcdb55bc414bf8657eb899a52
nhuhoa <htran@bccrc.ca>
2023-07-17 21:58:27 +0000

    Adding raw, normalized bulk RNA expression for mixing SA919

--- scripts/method_testing/run_TreeAlign/load_data_TreeAlign_utils.R
+++ scripts/method_testing/run_TreeAlign/load_data_TreeAlign_utils.R
@@ -32,7 +32,7 @@ load_10x_TreeAlign <- function(sce_fn, ens_obs_genes, sid, save_dir){
   # saveRDS(sce, paste0(save_dir, sid, '_introns_sce.rds'))
   # print(assayNames(sce))
   
-  mtx <- as.data.frame(counts(sce))
+  mtx <- as.data.frame(as.matrix(counts(sce)))
   # dim(mtx)
   print(rownames(mtx)[1])
   print(colnames(mtx)[1])
@@ -44,71 +44,6 @@ load_10x_TreeAlign <- function(sce_fn, ens_obs_genes, sid, save_dir){
   
   data.table::fwrite(mtx, paste0(save_dir, sid, '_expr.csv.gz'), row.names = T)
   print(dim(mtx))
-}
-
-
-cells_clone_fn <- '/home/htran/storage/datasets/drug_resistance/rna_results/manuscript/cell_clones/SA609_cell_clones_metadata.csv'
-cnv_fn <- '/home/htran/storage/raw_DLP/drug_resistance_DLP/SA609/reads_clones_v3/mapped_wholedata_SA609_grch38_v2.csv.gz'
-sids <- c('SA609X5XB03223','SA609X7XB03554')
-save_dir <- '/home/htran/storage/datasets/drug_resistance/rna_results/SA609_rna/TreeAlign_testing/'
-input_dir <- "/home/htran/storage/datasets/drug_resistance/rna_results/rnaseq_v6/SA609-v6/"
-# cnv contains ensembl_gene_id and list of clones, each clone is one column
-get_clones_per_sample <- function(cnv_fn, cells_clone_fn, sids, 
-                                  save_dir, min_cells_per_clone=10){
-  # cells_clone_fn <- '/home/htran/Projects/hakwoo_project/metastasis_material/materials/dlp_trees/SA535/cell_clones.csv.gz'
-  # save_dir <- '/home/htran/storage/datasets/metastasis_results/rnaseq_SA535/clonealign_introns/input_data/'
-  dir.create(save_dir)
-  
-  cnv <- data.table::fread(cnv_fn) %>% as.data.frame()
-  
-  cells_clone <- data.table::fread(cells_clone_fn)
-  colnames(cells_clone)
-  cells_clone$library_id <- get_library_id(cells_clone$cell_id)
-  cells_clone$sample_id <- get_sample_id(cells_clone$cell_id)
-  unique(cells_clone$sample_id)
-  unique(cells_clone$library_id)
-  
-  # meta <- data.table::fread('/home/htran/Projects/hakwoo_project/metastasis_material/materials/10x/SA535_10x_metadata_passage_X4_full_infos.csv')
-  # sids <- unique(meta$mouse_id)
-  # # 
-  
-  ## for TreeAlign shell script
-  writeLines(sids, paste0(save_dir, "samples_run_list.txt"))  
-  
-  unassigned <- c('None','Un','unassigned','Unassigned')
-  for(sid in sids){
-    print(sid)
-    
-    clone_used <- cells_clone %>% 
-      dplyr::filter(sample_id==sid & !clone_id %in% unassigned) %>%
-      dplyr::group_by(clone_id) %>%
-      dplyr::summarise(nb_cells=n()) %>%
-      dplyr::filter(nb_cells>=min_cells_per_clone)
-    
-    if(dim(clone_used)[1]>0){
-      obs_clones <- unique(clone_used$clone_id)
-      print(obs_clones)
-      cnv_sid <- get_cnv_sid(cnv, save_dir, sid, obs_clones,
-                  purity_thrs=0.4, avg_mappability_thrs=0.7)
-      colnames(cnv_sid)
-      cnv1 <- cnv_sid %>%
-        dplyr::select(all_of(c('ensembl_gene_id', obs_clones)))
-      colnames(cnv1) <- c('ensembl_gene_id', paste0('cell_',obs_clones))
-      data.table::fwrite(cnv1, paste0(save_dir, sid, '_clones_cnv.csv.gz'))  
-      
-      clones_df <- data.frame(cell_id=paste0('cell_',obs_clones),
-                              clone_id=obs_clones)
-      
-      data.table::fwrite(clones_df, paste0(save_dir,sid,'_cell_clones.csv.gz'))
-      sce_fn <- paste0(input_dir, sid,'.rdata')
-      if(file.exists(sce_fn)){
-        ens_obs_genes <- unique(cnv$ensembl_gene_id)
-        load_10x_TreeAlign(sce_fn, ens_obs_genes, sid, save_dir)  
-      }
-      
-    }
-  }  
-    
 }
 
 get_cnv_sid <- function(cnv, save_dir, sid, obs_clones,
@@ -178,6 +113,80 @@ get_cnv_sid <- function(cnv, save_dir, sid, obs_clones,
   cols_use <- gsub('cn_median_','',cols_use)
   colnames(cnv) <- cols_use
   return(cnv)
+  
+}
+
+cells_clone_fn <- '/home/htran/storage/datasets/drug_resistance/rna_results/manuscript/cell_clones/SA609_cell_clones_metadata.csv'
+cnv_fn <- '/home/htran/storage/raw_DLP/drug_resistance_DLP/SA609/reads_clones_v3/mapped_wholedata_SA609_grch38_v2.csv.gz'
+save_dir <- '/home/htran/storage/datasets/drug_resistance/rna_results/SA609_rna/TreeAlign_testing/'
+input_dir <- "/home/htran/storage/datasets/drug_resistance/rna_results/rnaseq_v6/SA609-v6/"
+# sids <- c('SA609X5XB03223','SA609X7XB03554')
+sids <- c('SA609X3XB01584','SA609X4XB003083','SA609X4XB03080',
+          'SA609X5XB03230','SA609X5XB03231','SA609X6XB03447')
+
+
+# cnv contains ensembl_gene_id and list of clones, each clone is one column
+get_clones_per_sample <- function(cnv_fn, cells_clone_fn, sids, 
+                                  save_dir, min_cells_per_clone=10){
+  # cells_clone_fn <- '/home/htran/Projects/hakwoo_project/metastasis_material/materials/dlp_trees/SA535/cell_clones.csv.gz'
+  # save_dir <- '/home/htran/storage/datasets/metastasis_results/rnaseq_SA535/clonealign_introns/input_data/'
+  min_cells_per_clone=10
+  if(!dir.exists(save_dir)){
+    dir.create(save_dir)  
+  }
+  cnv <- data.table::fread(cnv_fn) %>% as.data.frame()
+  cols <- colnames(cnv)
+  cols <- gsub('_R','_A',cols)
+  colnames(cnv) <- cols
+  cells_clone <- data.table::fread(cells_clone_fn)
+  colnames(cells_clone)
+  dim(cells_clone)
+  cells_clone$library_id <- get_library_id(cells_clone$cell_id)
+  cells_clone$sample_id <- get_sample_id(cells_clone$cell_id)
+  unique(cells_clone$sample_id)
+  unique(cells_clone$library_id)
+  
+  # meta <- data.table::fread('/home/htran/Projects/hakwoo_project/metastasis_material/materials/10x/SA535_10x_metadata_passage_X4_full_infos.csv')
+  # sids <- unique(meta$mouse_id)
+  # # 
+  
+  ## for TreeAlign shell script
+  writeLines(sids, paste0(save_dir, "samples_run_list.txt"))  
+  
+  unassigned <- c('None','Un','unassigned','Unassigned')
+  for(sid in sids){
+    print(sid)
+    
+    clone_used <- cells_clone %>% 
+      dplyr::filter(sample_id==sid & !clone_id %in% unassigned) %>%
+      dplyr::group_by(clone_id) %>%
+      dplyr::summarise(nb_cells=n()) %>%
+      dplyr::filter(nb_cells>=min_cells_per_clone)
+    
+    if(dim(clone_used)[1]>0){
+      obs_clones <- unique(clone_used$clone_id)
+      print(obs_clones)
+      cnv_sid <- get_cnv_sid(cnv, save_dir, sid, obs_clones,
+                  purity_thrs=0.4, avg_mappability_thrs=0.7)
+      colnames(cnv_sid)
+      cnv1 <- cnv_sid %>%
+        dplyr::select(all_of(c('ensembl_gene_id', obs_clones)))
+      colnames(cnv1) <- c('ensembl_gene_id', paste0('cell_',obs_clones))
+      data.table::fwrite(cnv1, paste0(save_dir, sid, '_clones_cnv.csv.gz'))  
+      
+      clones_df <- data.frame(cell_id=paste0('cell_',obs_clones),
+                              clone_id=obs_clones)
+      
+      data.table::fwrite(clones_df, paste0(save_dir,sid,'_cell_clones.csv.gz'))
+      sce_fn <- paste0(input_dir, sid,'.rdata')
+      if(file.exists(sce_fn)){
+        ens_obs_genes <- unique(cnv1$ensembl_gene_id)
+        load_10x_TreeAlign(sce_fn, ens_obs_genes, sid, save_dir)  
+      }
+      
+    }
+  }  
     
 }
+
 
---
+++ scripts/method_testing/run_TreeAlign/run_TreeAlign_SA609.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+input_dir=/home/htran/storage/datasets/drug_resistance/rna_results/SA609_rna/TreeAlign_testing/
+save_dir="${input_dir}/TreeAlign_result_thrs08/"
+script_exe=/home/htran/Projects/hakwoo_project/metastasis_material/scripts/method_testing/run_TreeAlign/exe_TreeAlign.py
+python_env=/home/htran/anaconda3/envs/sisyphus/bin/python
+meta_sample_ids="${input_dir}/samples_run_list.txt"
+
+## Read list of samples into an array
+IFS=$'\n' read -d '' -r -a datatags < $meta_sample_ids
+
+
+nb_iterations=500
+min_clone_assign_prob=0.8
+
+
+for ((n=0;n<${#datatags[@]};n++));
+do
+  echo "________________________________________________________"
+  printf "Testing sample: %s\n" "${datatags[n]}"
+  
+  
+  base_dir="${input_dir}"
+  [ ! -d "$base_dir" ] && mkdir -p "$base_dir"
+  # mkdir $base_dir
+  
+  expr_fn="${input_dir}${datatags[n]}_expr.csv.gz"
+  
+  cnv_fn="${input_dir}${datatags[n]}_clones_cnv.csv.gz"
+  
+  cell_clones_fn="${input_dir}${datatags[n]}_cell_clones.csv.gz"
+  
+  [ ! -d "$save_dir" ] && mkdir -p "$save_dir"
+  
+  echo "Running tree align"
+  
+  command_script="${python_env} ${script_exe} --expr_fn ${expr_fn} --cnv_fn ${cnv_fn} --cell_clones_fn ${cell_clones_fn} --datatag ${datatags[n]} --save_dir ${save_dir} --iterations ${nb_iterations} --assign_prob ${min_clone_assign_prob}"
+
+ 
+  echo $command_script
+  eval "$command_script"
+  echo "Done!"
+  
+  
+  
+done  
+
+echo "Completed!!!"
