ee47d22eb1aefe8cf9e9ccadd1cbd09813a0632b
Shuyu Fan <106266965+sfan-bccrc@users.noreply.github.com>
2024-03-26 15:55:37 -0700

    Replace SA_ID with patient_ID
    And use the color_code for each patient_ID.

--- materials/tumour_volumes/growth_curves_scripts_input_Shuyu/lmer_subscription_font_2024.R
+++ materials/tumour_volumes/growth_curves_scripts_input_Shuyu/lmer_subscription_font_2024.R
@@ -8,6 +8,7 @@ library(extrafont)
 
 
 temp_data <- subset(firstcurve_df, experiment_type == "main_experiment")
+patient_id <- read.csv(paste0(datadir, "March2024Data/",'mapping_SAID_PatientID_color.csv'))
 
 #number of unique tumors with main experiment
 length(unique(temp_data$Tumor_ID))
@@ -15,10 +16,14 @@ length(unique(temp_data$Tumor_ID))
 #number of unique tumors with main experiment
 temp_data <- subset(temp_data,
   Damian_annotation %in% c("non_met", "met"))
+
+temp_data <- merge(temp_data, patient_id, by = "SA_ID")
+
   
 #number of unique tumors with main experiment AND with non_met or met annotation.
 ## excluding unknown category
   length(unique(temp_data$Tumor_ID))
+  
   
 temp_data$Damian_annotation <- factor(temp_data$Damian_annotation, 
                                       levels = c("non_met",  "met"))
@@ -65,29 +70,60 @@ max_length = max(temp_data$date_len, na.rm = TRUE)
 max_size = max(temp_data$TumorVol_num)
 
 
+color_sa <- patient_id$color_code
+names(color_sa) <- patient_id$patient_ID
+
+
+
+my_font <- "Helvetica"
+lg_pos <- "top"  
+thesis_theme <- ggplot2::theme(  
+  text = element_text(color="black",size = 8, hjust = 0.5, family=my_font),  
+  axis.title.x = element_text(color="black",size=10, hjust = 0.5, family=my_font),  
+  axis.title.y = element_text(color="black",size=10, hjust = 0.5, family=my_font),  
+  axis.text.x = element_text(color="black",size=7, hjust = 0.5, family=my_font),#, angle = 90
+  # axis.text.x = element_blank(),  
+  axis.text.y = element_text(color="black",size=7, hjust = 0.5, family=my_font),  
+  plot.title = element_text(color="black",size=10, face="bold", hjust=0, family=my_font),  
+  legend.title=element_text(color="black",size=7, hjust = 0.5, family=my_font),  
+  legend.text=element_text(color="black",size=7, hjust = 0.5, family=my_font),  
+  strip.text.x = element_text(color="black",size=10, family=my_font),  
+  strip.text.y = element_text(color="black",size=10, family=my_font),  
+  legend.spacing.x = unit(0.1, 'mm'),  
+  legend.spacing.y = unit(0.1, 'mm'),  
+  legend.key.height=unit(0.5,"line"),  
+  legend.position = lg_pos,  
+  legend.background = element_blank(),
+  panel.grid.major = element_blank(), panel.grid.minor = element_blank()  
+)
+
 p <- ggplot(data = temp_data, aes(x = date_len, 
                                   y = TumorVol_num^(1/3),
-                                  
                                   group=Tumor_ID)) +
-  geom_point(size = 1.5, alpha=.3) +geom_line(linetype = 2, size = 1, alpha=.3) + #theme(legend.position = "none") +
-  geom_line(aes(y=predm2fit,group=Tumor_ID, colour = SA_ID), size =1.2,  alpha=.6)+
+  geom_point(size = 1.5, alpha=.3) +
+  geom_line(linetype = 2, size = 1, alpha=.3) + 
+  geom_line(aes(y=predm2fit,group=Tumor_ID, colour = patient_ID), size =1.2,  alpha=.6)+ # note: color by patient id
   scale_x_continuous(limits = c(0, max_length), name = "Days") +
   scale_y_continuous(limits = c(0, max_size^(1/3)), 
                      name = expression(paste(sqrt("Volume", 3)))) +#, trans = "log")#+
   
   labs(colour="Patient ID")+ 
   scale_color_manual(values=color_sa)+
-  facet_wrap(vars(Damian_annotation), dir = "v")+
-  theme(
-    axis.text.x = element_text(family = "Helvetica", size = 8),
-    axis.text.y = element_text(family = "Helvetica", size = 8),
-    axis.title.x = element_text(family = "Helvetica", size = 10),
-    axis.title.y = element_text(family = "Helvetica", size = 10),
-    strip.text.x = element_text(family = "Helvetica", size = 12),
-    legend.title=element_text(family = "Helvetica", size = 12),
-    legend.text = element_text(family = "Helvetica", size = 10))
+  facet_wrap(vars(Damian_annotation), dir = "h")+ ## note: using horizontal order
+  thesis_theme + 
+  guides(colour = guide_legend(nrow = 1)) # legends in one row
 
-p <- p + geom_line(colour="red",aes(y=predm2, x=date_len), linewidth = 2, alpha=.5,
+# theme(
+#   axis.text.x = element_text(family = "Helvetica", size = 8),
+#   axis.text.y = element_text(family = "Helvetica", size = 8),
+#   axis.title.x = element_text(family = "Helvetica", size = 10),
+#   axis.title.y = element_text(family = "Helvetica", size = 10),
+#   strip.text.x = element_text(family = "Helvetica", size = 12),
+#   legend.title=element_text(family = "Helvetica", size = 12),
+#   legend.text = element_text(family = "Helvetica", size = 10))
+
+p <- p + geom_line(colour="red",aes(y=predm2, x=date_len), 
+                   linewidth = 1.7, alpha=.6,
                    inherit.aes = F)
 print(p)
 
@@ -102,11 +138,14 @@ dev.off()
 ## the device is used to avoid 
 # "In grid.Call.graphics(C_points, x$x, x$y, x$pch, x$size) :
 #   semi-transparency is not supported on this device: reported only once per page"
-ggsave(filename=paste0(resultdir, "tumor_main_cubic_helvetica.eps"),  
-       height = 3.5,
-       width = 4,units = "in",  device=cairo_ps)
-print(p)
-dev.off()
+
+ggsave(paste0(resultdir,"tumor_main_cubic_helvetica.svg"),  
+       plot = p,  
+       height = 3.3,  
+       width = 7.5,  
+       # useDingbats=F,  
+       dpi=200  
+)
 
 
 fit3 <- lmer(TumorVol_num^(1/3) ~ date_len*Damian_annotation+
@@ -118,8 +157,8 @@ fit3 <- lmer(TumorVol_num^(1/3) ~ date_len*Damian_annotation+
 summary3_stat = summary(fit3)
 round(summary3_stat$coefficients, 3)
 
-SuppTable3 <- temp_data[, c("Tumor_ID", "SA_ID","Damian_annotation",
+SuppTable3 <- temp_data[, c("Tumor_ID","patient_ID", "Damian_annotation",
                                   "PTR_Date", "TumorDate_Date",
-                                  "TumorVol_num", "date_len")]
+                                  "TumorVol_num", "date_len", "color_code")]
 
 write.csv(SuppTable3, paste0(resultdir, "SuppTable3.csv"))
